8d0eeed59dbb04653251a38403de095f3cc17e3e
Md. Hanif <mdhanif6731@gmail.com>
2020-08-08 12:53:15 +0600

    doctor registration view added

--- controllers/registration.js
+++ controllers/registration.js
@@ -9,6 +9,9 @@ router.get('/',function (req,res){
 router.get('/doctor',function (req,res){
     res.render('registration/doctor');
 });
+router.post('/doctor',function (req,res){
+    res.send('yo.. sup!');
+});
 
 
 
